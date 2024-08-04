473216abb6f3ac0ace2ccecd488f81ef344f170a
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-08-03 20:37:36 -0400

    last minute change

--- db/client.cjs
+++ db/client.cjs
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 const { Client } = require('pg');
 
+// I get a 'password must be a string' error if i dont include the name/password to get into databse
 const client = new Client(process.env.DATABASE_URL || 'postgress://Chamb:fullstack@localhost:5432/acme_employees');
 
 module.exports = client;
